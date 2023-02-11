31a131de28944b7bcc52935a028ac1f4b9dc7236
b.tursunkulov <@Zhakypali.Ayaulym>
2023-02-11 16:43:04 +0600

    change

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -5,6 +5,11 @@ export default function HomePage() {
     return (
         <>
         <header className="gdl-header">
+            <h1 class="gdl-header__logo">
+            <Link to="/home">
+                 <img src="https://digitallibrary.io/wp-content/themes/gdl-theme-twentytwentytwo/assets/logo-grey.svg" alt="Logotype for Global Digital Library" aria-hidden="true" width="217" height="62"/>
+                 </Link>
+            </h1>
             <div className="gdl-header__usernav">
             <h1>
             <svg class="svg-icon" width="28" height="28" vertical-align="middle" fill="#596d79" overflow="hidden" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg"><path d="M921.6 454.656H223.232c-83.968 0-151.552-75.776-151.552-167.936s67.584-167.936 151.552-167.936H921.6c16.384 0 30.72 14.336 30.72 30.72v6.144c0 12.288-8.192 24.576-20.48 28.672-40.96 14.336-69.632 53.248-69.632 100.352 0 45.056 28.672 88.064 69.632 100.352 12.288 4.096 20.48 16.384 20.48 28.672v6.144c0 22.528-14.336 34.816-30.72 34.816zM223.232 182.272c-49.152 0-90.112 47.104-90.112 106.496s40.96 106.496 90.112 106.496h612.352c-22.528-28.672-34.816-65.536-34.816-106.496 0-38.912 12.288-75.776 34.816-106.496H223.232zM800.768 903.168H102.4c-16.384 0-30.72-14.336-30.72-30.72v-6.144c0-14.336 8.192-24.576 20.48-28.672 40.96-14.336 69.632-55.296 69.632-100.352 0-47.104-28.672-88.064-69.632-100.352-12.288-4.096-20.48-16.384-20.48-28.672v-6.144c0-16.384 14.336-30.72 30.72-30.72h698.368c83.968 0 151.552 75.776 151.552 167.936s-67.584 163.84-151.552 163.84z m-612.352-61.44h612.352c49.152 0 90.112-47.104 90.112-106.496s-40.96-106.496-90.112-106.496H188.416c22.528 28.672 34.816 65.536 34.816 106.496 0 38.912-12.288 75.776-34.816 106.496z"  /></svg>
@@ -27,11 +32,26 @@ export default function HomePage() {
             </span>
             </div>
         </header>
-            <div className="text-center">
+            <div className="wp-block-create-block-wrapper gdl-container desktop-only">
+                <div className="wp-block-media-text alignwide is-stacked-on-mobile">
+                  <figure className="wp-block-media-text__media">
+                   <img decoding="async" loading="lazy" width="700" height="600" src="https://digitallibrary.io/wp-content/uploads/2022/10/GDL-front-illustration-1-1.jpeg" alt="" class="wp-image-27118 size-full" srcset="https://digitallibrary.io/wp-content/uploads/2022/10/GDL-front-illustration-1-1.jpeg 800w, https://digitallibrary.io/wp-content/uploads/2022/10/GDL-front-illustration-1-1-300x200.jpeg 300w, https://digitallibrary.io/wp-content/uploads/2022/10/GDL-front-illustration-1-1-768x512.jpeg 768w, https://digitallibrary.io/wp-content/uploads/2022/10/GDL-front-illustration-1-1-50x33.jpeg 50w, https://digitallibrary.io/wp-content/uploads/2022/10/GDL-front-illustration-1-1-100x67.jpeg 100w" sizes="(max-width: 800px) 100vw, 800px"/>
+                  </figure>
+                <div className="wp-block-media-text__content">
+                    <p  class="has-text-color">
+                    <strong>The Global Digital Library</strong> provides free, high-quality educational resources for anyone, anywhere.
+                    </p>
+                </div>
+                </div>
+            </div>
+
+            {/* <div className="text-center">
                 <h1 className="main-title home-page-title">welcome to our app</h1>
                 <Link to="/">
                     <button className="primary-button">Log out</button>
                 </Link>
-            </div></>
+            </div> */}
+            
+        </>
     )
 }
