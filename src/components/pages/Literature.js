5f00f8c4f5f09429da718eee7be384d7a7ecca87
b.tursunkulov <@Zhakypali.Ayaulym>
2023-02-26 22:50:10 +0600

    update

--- src/components/pages/Literature.js
+++ src/components/pages/Literature.js
@@ -15,6 +15,92 @@ export default function Genres() {
     
 
     return (
+        <>
+        <div>
+            <h1 align="center">Literature</h1>
+        </div>
+        <div class="products-grid grid">
+                
+			  	<figure class="product-style">
+					<img src="https://readrate.com/img/pictures/book/295/29554/29554/w240h400-d7dc49bc.jpg" alt="Books" class="product-item"/>
+					<figcaption>
+						<h3>Portrait photography</h3>
+						<p>Adam Silber</p>
+						<div class="item-price">$ 40.00</div>
+					</figcaption>
+				</figure>
+
+			  	<figure class="product-style">
+					<img src="images/tab-item2.jpg" alt="Books" class="product-item"/>
+					<button type="button" class="add-to-cart" data-product-tile="add-to-cart">Add to Cart</button>
+					<figcaption>
+						<h3>Once upon a time</h3>
+						<p>Klien Marry</p>
+						<div class="item-price">$ 35.00</div>
+					</figcaption>
+				</figure>
+
+			  	<figure class="product-style">
+					<img src="images/tab-item3.jpg" alt="Books" class="product-item"/>
+					<button type="button" class="add-to-cart" data-product-tile="add-to-cart">Add to Cart</button>
+					<figcaption>
+						<h3>Tips of simple lifestyle</h3>
+						<p>Bratt Smith</p>
+						<div class="item-price">$ 40.00</div>
+					</figcaption>
+				</figure>
+
+			  	<figure class="product-style">
+					<img src="images/tab-item4.jpg" alt="Books" class="product-item"/>
+					<button type="button" class="add-to-cart" data-product-tile="add-to-cart">Add to Cart</button>
+					<figcaption>
+						<h3>Just felt from outside</h3>
+						<p>Nicole Wilson</p>
+						<div class="item-price">$ 40.00</div>
+					</figcaption>
+				</figure>
+
+			  	<figure class="product-style">
+					<img src="images/tab-item5.jpg" alt="Books" class="product-item"/>
+					<button type="button" class="add-to-cart" data-product-tile="add-to-cart">Add to Cart</button>
+					<figcaption>
+						<h3>Peaceful Enlightment</h3>
+						<p>Marmik Lama</p>
+						<div class="item-price">$ 40.00</div>
+					</figcaption>
+				</figure>
+
+			  	<figure class="product-style">
+					<img src="images/tab-item6.jpg" alt="Books" class="product-item"/>
+					<button type="button" class="add-to-cart" data-product-tile="add-to-cart">Add to Cart</button>
+					<figcaption>
+						<h3>Great travel at desert</h3>
+						<p>Sanchit Howdy</p>
+						<div class="item-price">$ 40.00</div>
+					</figcaption>
+				</figure>
+
+			  	<figure class="product-style">
+					<img src="images/tab-item7.jpg" alt="Books" class="product-item"/>
+					<button type="button" class="add-to-cart" data-product-tile="add-to-cart">Add to Cart</button>
+					<figcaption>
+						<h3>Life among the pirates</h3>
+						<p>Armor Ramsey</p>
+						<div class="item-price">$ 40.00</div>
+					</figcaption>
+				</figure>
+
+			  	<figure class="product-style">
+					<img src="images/tab-item8.jpg" alt="Books" class="product-item"/>
+					<button type="button" class="add-to-cart" data-product-tile="add-to-cart">Add to Cart</button>
+					<figcaption>
+						<h3>Simple way of piece life</h3>
+						<p>Armor Ramsey</p>
+						<div class="item-price">$ 40.00</div>
+					</figcaption>
+				</figure>
+
+	    	</div>
         <div className="text-center">
 
             {/* <div>
@@ -24,10 +110,10 @@ export default function Genres() {
                     </div>
                 ))}
             </div> */}
-            <h1 className="main-title home-page-title">Literature</h1>
-            <Link to="/">
-                <button className="primary-button">Log out</button>
+            <Link to="/home">
+                <button className="primary-button">Back to Home</button>
             </Link>
         </div>
+        </>
     )
 }
