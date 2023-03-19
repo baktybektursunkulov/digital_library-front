4a8a4f292048d580ff06dcee2df46e35878098a5
b.tursunkulov <@Zhakypali.Ayaulym>
2023-03-19 21:55:19 +0600

    update

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,22 +1,53 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom'
+import { useHistory } from 'react-router-dom';
 
 import '../../App.css'
 
 export default function SignInPage() {
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const history = useHistory();
+  
+    const handleSubmit = async (event) => {
+      event.preventDefault();
+  
+      try {
+        const response = await fetch('http://localhost:8075/api/v1/auth/login', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ username, password }),
+        });
+        console.log(response)
+        if (response.ok) {
+            console.log("test")
+          const data = await response.json();
+          history.push('/home');
+          // do something with the data
+        } else {
+          throw new Error('Invalid email or password');
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
     return (
         <div className="text-center m-5-auto">
             <h2 className="login_h2">Sign in to us</h2>
-            <form action="/home">
+            <form onSubmit={handleSubmit} action="/home">
+            <p>
+                    <label>Username</label><br/>
+                    <input type="text" id="username" value={username} onChange={(event) => setUsername(event.target.value)} />
+                    </p>
                 <p>
-                    <label>Username or email address</label><br/>
-                    <input type="text" name="first_name" required />
-                </p>
-                <p>
-                    <label>Password</label>
+                <label>Password</label>
                     <Link to="/forget-password"><label className="right-label">Forget password?</label></Link>
                     <br/>
-                    <input type="password" name="password" required />
+                    <input type="password" id="password" value={password} onChange={(event) => setPassword(event.target.value)} />
                 </p>
                 <p>
                     <button id="sub_btn" type="submit">Login</button>
--- src/components/pages/RegisterPage.js
+++ src/components/pages/RegisterPage.js
@@ -1,26 +1,67 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom'
 
 import '../../App.css'
 
 export default function SignUpPage() {
 
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [email, setEmail] = useState('');
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        try {
+            const response = await fetch('http://localhost:8075/api/v1/auth/logup', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ username,firstName,lastName, email,password })
+            });
+
+            if (!response.ok) {
+                const errorData = await response.json();
+                setErrorMessage(errorData.message);
+            } else {
+                // Redirect the user to the login page upon successful signup
+                window.location.href = '/login';
+            }
+        } catch (error) {
+            console.error(error);
+            setErrorMessage('An error occurred. Please try again later.');
+        }
+    };
+
     return (
         <div className="text-center m-5-auto">
             <h2>Join us</h2>
             <h5>Create your personal account</h5>
-            <form action="/home">
+            <form onSubmit={handleSubmit}>
+                {errorMessage && <div>{errorMessage}</div>}
                 <p>
-                    <label>Username</label><br/>
-                    <input type="text" name="first_name" required />
+                    <label>Username</label><br />
+                    <input type="text" value={username} onChange={(event) => setUsername(event.target.value)} />
                 </p>
                 <p>
                     <label>Email address</label><br/>
-                    <input type="email" name="email" required />
+                    <input type="email" value={email} onChange={(event) => setEmail(event.target.value)} />
                 </p>
                 <p>
-                    <label>Password</label><br/>
-                    <input type="password" name="password" requiredc />
+                    <label>First name</label><br />
+                    <input type="text" value={firstName} onChange={(event) => setFirstName(event.target.value)} />
+                </p>
+                <p>
+                    <label>Last name</label><br />
+                    <input type="text" value={lastName} onChange={(event) => setLastName(event.target.value)} />
+                </p>
+                <p>
+                    <label>Password</label><br />
+                    <input type="password" value={password} onChange={(event) => setPassword(event.target.value)} />
                 </p>
                 <p>
                     <input type="checkbox" name="checkbox" id="checkbox" required /> <span>I agree all statements in <a href="https://google.com" target="_blank" rel="noopener noreferrer">terms of service</a></span>.
