6a36ee0e7037d668a57e31e58311800f905e0804
b.tursunkulov <@Zhakypali.Ayaulym>
2023-03-28 15:06:59 +0600

    update

--- src/components/pages/ResetPasswordPage.js
+++ src/components/pages/ResetPasswordPage.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect,axios } from 'react';
+import React, { useState, useEffect, axios } from 'react';
 import { Link } from 'react-router-dom'
 import { useHistory } from 'react-router-dom';
 
@@ -9,46 +9,48 @@ export default function ResetPassword() {
   const [confirmPassword, setConfirmPassword] = useState('');
   const [error, setError] = useState(null);
   const history = useHistory();
-  
-  const handleSubmit = async(e) => {
-  e.preventDefault();
-  const token = new URLSearchParams(window.location.search).get('token');
-  try {
-    const response = await fetch('http://localhost:8075/api/password/reset-password', {
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ token, password }),
-    });
-    console.log(response)
-    if (response.ok) {
-      console.log('Password reset successfully:', response);
-      // const data = await response.json();
-      //   localStorage.setItem('myData', JSON.stringify(data));
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const token = new URLSearchParams(window.location.search).get('token');
+    try {
+      const response = await fetch('http://localhost:8075/api/password/reset-password', {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ token, password }),
+      });
+      console.log(response)
+      if (response.ok) {
+        console.log('Password reset successfully:', response);
+        // const data = await response.json();
+        //   localStorage.setItem('myData', JSON.stringify(data));
         history.push('/login');
-      // do something with the data
-    } else {
-      throw new Error('Invalid email or password');
+        // do something with the data
+      } else {
+        throw new Error('Invalid email or password');
+      }
+    } catch (error) {
+      console.error('Error resetting password:', error);
+      setError(error.response.data.message);
     }
-  } catch (error) {
-    console.error('Error resetting password:', error);
-  setError(error.response.data.message);
   }
-  }
-  
+
   return (
-  <form onSubmit={handleSubmit}>
-  {error && <div>{error}</div>}
-  <label>
-  New password:
-  <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
-  </label>
-  <label>
-  Confirm new password:
-  <input type="password" value={confirmPassword} onChange={e => setConfirmPassword(e.target.value)} />
-  </label>
-  <button type="submit">Submit</button>
-  </form>
+    <form onSubmit={handleSubmit}>
+      {error && <div>{error}</div>}
+      <p>
+      <label>New password:</label><br />
+        <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+      </p>
+      <p>
+      <label>Confirm new password:</label><br />
+        <input type="password" value={confirmPassword} onChange={e => setConfirmPassword(e.target.value)} />
+      </p>
+      <p>
+      <button type="submit">Submit</button>
+      </p>
+    </form>
   );
-  }
+}
